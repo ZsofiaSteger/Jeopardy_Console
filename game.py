a45d66e51306fb61904bceb3183638ee588ba3fd
ZsofiaSteger <152066591+ZsofiaSteger@users.noreply.github.com>
2023-12-13 22:30:53 +0100

    add panel formatting

--- game.py
+++ game.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from thefuzz import fuzz
-from rich import print as r_print
+from rich.console import Console
+from rich.panel import Panel
+from rich import box
+
+console = Console()
 
 def load_data():
     jeopardy_df = pd.read_csv("data/jeopardy.csv")
@@ -18,15 +22,15 @@ def get_active_questions(jeopardy_df):
 def display_questions(active_questions):
     i = 1
     for active_question in active_questions.itertuples():
-        print(f'{i}. Value: {active_question.value}, Category: {active_question.category}')
+        console.print(Panel(f'[bold cyan]{active_question.value}[/bold cyan], [bold bright_green]Category: {active_question.category}[/bold bright_green]', title=f'Question {i}', title_align='left', width=60))
         i += 1
 
 def check_answer(user_answer, correct_answer):
     if fuzz.ratio(user_answer, correct_answer) > 80:
-        r_print(f'[bold green]Correct![/bold green] The answer was: {correct_answer}')
+        console.print(f'[bold green]Correct![/bold green] The answer was: {correct_answer}')
         return True
     else:
-        r_print(f'[bold red]Incorrect.[/bold red] The correct answer was: {correct_answer}')
+        console.print(f'[bold red]Incorrect.[/bold red] The correct answer was: {correct_answer}')
         return False
 
 def get_score(answer_status, old_score, answer_value):
@@ -36,7 +40,7 @@ def get_score(answer_status, old_score, answer_value):
         return old_score - answer_value
 
 def get_quiz(number_of_rounds):
-    score = 0
+    score = 1000
     jeopardy_df = load_data()
     for round in range(0, number_of_rounds):
         if score < 0:
@@ -47,17 +51,16 @@ def get_quiz(number_of_rounds):
         print("Choose your question:")
         display_questions(active_questions)
         
-        chosen_one = input("Enter the number of the question you want to answer:")
+        chosen_one = input("Enter the number of the question you want to answer: ")
         chosen_one = int(chosen_one) - 1
-        print(f'The question is: {active_questions.iloc[chosen_one].question}')
+        console.print(Panel(f'[bold magenta]{active_questions.iloc[chosen_one].question}[/bold magenta]', title='The question is:', title_align='left', width=90))
 
         user_answer = input("Enter your answer: ")
         answer_status = check_answer(user_answer, active_questions.iloc[chosen_one].answer)
-        score = get_score(answer_status, score, int(active_questions.iloc[chosen_one].value.replace('$', '')))
-        
-        print(f'Your score is: {score}')
+        score = get_score(answer_status, score, int(active_questions.iloc[chosen_one].value.replace('$', '').replace(',', '')))
+        console.print(Panel(f'[bold yellow]Your score is: {score}[/bold yellow]'))
     
     return score
 
 if __name__ == "__main__":
-    print(f'Your final score is: {get_quiz(2)}')
+    print(f'Your final score is: {get_quiz(5)}')
